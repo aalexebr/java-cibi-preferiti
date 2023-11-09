1484fa456cc662309ca657ea634bd2ab7309dc88
aalexebr <alex.ebr@icloud.com>
2023-11-09 17:17:04 +0100

    'progress'

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -3,5 +3,15 @@ package org.lessons.java.array;
 public class CibiPreferiti {
 	public static void main(String[] args) {
 		System.out.println("test9");
+		
+		String[] foodArr = {"mango","oranage","maracuja","banana","grape"};
+		
+		int arrLength = foodArr.length;
+		String fave = foodArr[0];
+		String last = foodArr[arrLength-1];
+		
+		System.out.println("length: "+arrLength);
+		System.out.println("favorite food: "+fave);
+		System.out.println("least favorite food: "+last);
 	}
 }
