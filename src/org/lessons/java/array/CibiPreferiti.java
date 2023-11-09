5b85cf8aedc0861752aecce98d6a37396d472985
aalexebr <alex.ebr@icloud.com>
2023-11-09 17:33:55 +0100

    bonus

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -13,5 +13,16 @@ public class CibiPreferiti {
 		System.out.println("length: "+arrLength);
 		System.out.println("favorite food: "+fave);
 		System.out.println("least favorite food: "+last);
+		
+		String middle = foodArr[arrLength/2];
+		double x = (double) arrLength/2;
+		System.out.println("test decimal division: "+x);
+		System.out.println("food in the middle with odd lenght array: "+middle);
+		
+		String[] foodArr2 = {"mango","oranage","maracuja","banana","grape","berry"};
+		int arrLength2 = foodArr2.length;
+		String middle2 = foodArr2[arrLength2/2];
+		String middle3 = foodArr2[(arrLength2/2) -1];
+		System.out.println("foods in the middle with even lenght array: "+middle2+" and "+middle3);
 	}
 }
